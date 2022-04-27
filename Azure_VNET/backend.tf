7bfcbcc55bdebeec23a1b7167bc60ddf0304b080
Rinkesh Putta <Putta_r@India.XoriantCorp.com>
2022-04-27 10:56:18 +0530

    this is a initial commit

---
+++ Azure_VNET/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 2.98.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "random" {
+  #Add your configuration
+}
---
+++ Azure_VNET/outputs.tf
@@ -0,0 +1,23 @@
+output "rg_name" {
+    value = "azurerm_resource_group.training.name"
+}
+
+output "rg_id" {
+    value = "azurerm_resource_group.training.id"
+}
+
+output "storage_acc_name" {
+    value = "azurerm_storage_account.trainingstorage.name"
+}
+
+output "storage_acc_id" {
+    value = "azurerm_storage_account.trainingstorage.id"
+}
+
+output "virtual_machine_name" {
+    value = "azurerm_windows_virtual_machine.zomatovm1.name"
+}
+
+output "virtual_machine_id" {
+    value = "azurerm_windows_virtual_machine.zomatovm1.id"
+}
